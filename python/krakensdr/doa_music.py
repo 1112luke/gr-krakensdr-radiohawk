0136bcafa9abf590c29b20c4f7fdc70201372315
Luke Scholler <lukescholler@gmail.com>
2025-06-16 09:45:28 -0400

    added func'

--- python/krakensdr/doa_music.py
+++ python/krakensdr/doa_music.py
@@ -17,7 +17,7 @@ class doa_music(gr.sync_block):
     """
     docstring for block doa_music
     """
-    def __init__(self, vec_len=1048576, freq=433.0, array_dist=0.33, num_elements=5, array_type='UCA'):
+    def __init__(self, vec_len=1048576, freq=433.0, array_dist=0.33, num_elements=5, array_type='UCA', processing_alg="MUSIC"):
         gr.sync_block.__init__(self,
             name="DOA MUSIC",
             in_sig=[(np.complex64, vec_len)] * num_elements,
